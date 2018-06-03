7ba7c459ac38a579c2d40aee4cf3ff4060e69a3e
Spongebob19 <39876190+Spongebob19@users.noreply.github.com>
2018-06-03 20:01:42 +0800

    Add files via upload

---
+++ assign1.py
@@ -0,0 +1,4 @@
+    Sessions_Attended = '1011,2344,3222,44322,555,6332,721,8789,99,1011,1124,1245,137,1499'
+data = Sessions_Attended.split(",")
+print("I have attended {} sessions!".format(len(data)))
+
